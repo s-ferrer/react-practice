340492fa0d722979cf30c39f4ced154e43d548da
s-ferrer <saraferrer@gmail.com>
2022-06-17 18:38:18 +0200

    Style practice 01

--- practice01/src/App.js
+++ practice01/src/App.js
@@ -1,10 +1,11 @@
 import "./style.css";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Main />
     </div>
