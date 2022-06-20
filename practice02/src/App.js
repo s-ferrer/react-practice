ce51817f5ce79facd98d9f9d171814490684c517
s-ferrer <saraferrer@gmail.com>
2022-06-20 17:22:33 +0200

    Create Navbar component

---
+++ practice02/src/App.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import React from "react";
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ practice02/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logoarbnb from "../images/Airbnb_Logo_Bélo.svg.png";
+
+export default function Navbar() {
+  <nav>
+    <img scr={logoarbnb} alt="logo" className="nav-log" />
+  </nav>;
+}
